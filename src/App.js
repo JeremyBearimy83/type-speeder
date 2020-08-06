b185dbd07a2cd2d4ba66f2cb63717d9df3ce1870
Ujjwal Singhal <ujjwals@gmail.com>
2020-08-06 18:56:40 +0530

    Add some basic functionality, still has a lot of bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import KeyboardEventHandler from "react-keyboard-event-handler";
+import React, { useState } from "react";
 import "./App.css";
+import text from "./text";
+import KeyboardEventHandler from "react-keyboard-event-handler";
 function App() {
+  const [index, setIncrement] = useState(0);
+  const textArr = text.split("");
   return (
     <div className="App">
+      <p
+        style={{
+          margin: "30px auto",
+          width: "50ch",
+          fontSize: "24px",
+          backgroundColor: "yellow",
+          lineHeight: "40px",
+          letterSpacing: "4px",
+        }}
+      >
+        {text}
+      </p>
+
       <KeyboardEventHandler
-        handleKeys={["alphabetic"]}
-        onKeyEvent={(key, e) => console.log(`You pressed ${key}`)}
+        handleKeys={textArr[index] === " " ? ["space"] : [textArr[index]]}
+        onKeyEvent={(key, e) => {
+          console.log(key, index);
+          setIncrement(index + 1);
+        }}
       />
     </div>
   );
